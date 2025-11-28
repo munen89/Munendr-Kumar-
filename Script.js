e6c17ab358861cf690e7e33be244bb09fcd519a6
munen89 <yadavmunendr9@gmail.com>
2025-11-28 12:16:50 +0530

    Dhanyawad munendr yadav digital marketing institute ki traph se essage function to handle form submission

---
+++ Script.js
@@ -0,0 +1,10 @@
+function sendMessage(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    document.getElementById("response-message").innerText =
+        `धन्यवाद ${name}! आपका संदेश भेज दिया गया है।`;
+
+    document.querySelector(".contact-form").reset();
+}
